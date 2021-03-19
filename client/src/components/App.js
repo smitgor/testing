5898a431e53089ef6b0db59cf2a5620d55192543
smitgor <smitgor120@gmail.com>
2021-03-19 18:20:31 +0530

    added simple header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Header from './header'
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Smit Gor</h1>
+        <Header dark={true}>test only</Header>
       </div>
     )
   }
